9f8a1dd0df4d920982d6b56884ebd62950d9e814
John Mayou <johnm.ceo@gmail.com>
2023-02-02 21:22:26 -0600

    page layout added, no functionality yet. Add button appends, new color btns to header

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+// database
+description VARCHAR(120)
+isComplete BOOLEAN
+timeStampCreated TIME
+timeCompleted TIME
+
+// table fields
+title
+description
+time completed
+complete / are you sure + go back /recover
+edit
+delete
+
+// inputs
+description
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,38 @@
-console.log("client.js ready");
-
 $(document).ready(onReady);
 
 function onReady() {
-	console.log("in jquery.js");
+	$("#show-color-btns").on("click", handleToggleColorButton);
+}
+
+// State
+let showColorButtons = false;
+
+function handleToggleColorButton() {
+	if (showColorButtons) {
+		showColorButtons = false;
+	} else {
+		showColorButtons = true;
+	}
+	render();
+}
+
+function renderColorBtns() {
+	return `
+        <button id="yellow-btn"></button>
+        <button id="orange-btn"></button>
+        <button id="purple-btn"></button>
+        <button id="blue-btn"></button>
+        <button id="green-btn"></button>
+    `;
+}
+
+function render() {
+	if (showColorButtons) {
+		$("show-color-btns").text("-");
+		$("#color-btn-list").empty();
+		$("#color-btn-list").append(renderColorBtns());
+	} else {
+		$("show-color-btns").text("+");
+		$("#color-btn-list").empty();
+	}
 }
